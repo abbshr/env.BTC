8443bb1cc73a4358802093b3e3fee0926c9584e0
Ran <abbshr@outlook.com>
2014-10-19 17:50:17 +0800

    fix bug in address query

--- util/sync_db.js
+++ util/sync_db.js
@@ -1,68 +1,49 @@
+#!/usr/bin/env node
+
 var path = require('path');
 
 function sync(db) {
-  return function (Address) {
-    console.log('Address Sync ...');
-    var level = require('levelup');
-    var odb_path, ndb_path = path.join(process.cwd(), '..', 'insight-db/addr');
-    /* origin db */
-    var odb = db;
-    /* new db */
-    var ndb = level(ndb_path);
+  
+  console.log('Address Sync ...');
+  var level = require('levelup');
+  var odb_path, ndb_path = path.join(__dirname, '../../../../', 'insight-db/addrs');
+  /* origin db */
+  var odb = db;
+  /* new db */
+  var ndb = level(ndb_path);
 
-    if (!db) {
-      odb_path = process.argv[2] || path.join(process.cwd(), '..', 'insight-db/txs');
-      odb = level(odb_path);
-    } else {
-      odb = db;
-    }
-    
-    var ndbws = ndb.createWriteStream()
-                .on('error', function (err) { console.log(err); })
-                .on('close', function () { console.log('WriteStream Closed'); });
-
-    //var addr_set = [];
-
-    var syncToDb = function (data) {
-      // sync logic
-      var addr = data.split('-')[1];
-      //if (addr_set.indexOf(addr) == -1) {
-        //addr_set.push(addr);
-        var a = new Address(addr);
-        a.update(function (err) {
-          if (err)
-            console.log(err);
-          else
-            ndbws.write({ 
-            key: addr, 
-            value: {
-              balance: a.balanceSat,
-              totalRecv: a.totalReceivedSat,
-              totalSpent: a.totalSentSat
-            } 
-          });
-          console.log('save address:', addr);
-        });
-      //}
-    };
-
-    odb.createKeyStream({
-        start: 'txa2-'
-      })
-      .on('data', function (d) {
-        syncToDb(d);
-      })
-      .on('error', function (err) {
-        err && console.log(err);
-      })
-      .on('end', function () {
-        ndbws.end();
-        console.log('Read End');
-      })
-      .on('close', function () {
-        console.log('ReadStream Closed');
-      });
+  var syncToDb = function (data) {
+    // sync logic
+    var addr = data.split('-')[1];
+    ndb.put(addr, 0);
+    console.log('save address:', addr);
   };
+
+  if (!db) {
+    odb_path = process.argv[2] || path.join(__dirname, '../../../../', 'insight-db/txs');
+    odb = level(odb_path);
+  } else {
+    odb = db;
+  }
+
+  odb.createKeyStream({
+      start: 'txa2-', 
+      end: 'txa2-~'
+    })
+    .on('data', function (d) {
+      syncToDb(d);
+    })
+    .on('error', function (err) {
+      err && console.log(err);
+    })
+    .on('end', function () {
+      ndbws.end();
+      console.log('Read End');
+    })
+    .on('close', function () {
+      console.log('ReadStream Closed');
+    });
 }
 
 module.exports = sync;
+sync();
