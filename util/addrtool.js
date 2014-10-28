e23f7d5751c7ee28add736ade853d6bef9ea24eb
ran <abbshr@outlook.com>
2014-10-28 12:41:37 +0800

    fix bug in ADDressDb.js

--- util/addrtool.js
+++ util/addrtool.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 var addressDb = require('../lib/AddressDb');
+
+var fs = require('fs');
+
 addressDb.historicSync();
+/*addressDb.getTopNAddress(100, function (addrs) {
+  addrs.forEach(function (addr) {
+    fs.appendFileSync('./top100', addr.addrStr + '\n');
+  });
+});*/
